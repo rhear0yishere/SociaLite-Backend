1e42e8c7aa41e9a0037ff95c6a6d5cb81f7d9b4f
Rhea Roy <rroy.rhea@gmail.com>
2019-04-10 22:55:30 -0700

    uncommented out middlewear?

--- server.js
+++ server.js
@@ -8,32 +8,6 @@ const eventRoutes = require('./routes/event');
 const cors=  require ('cors');
 
 
-const Storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, './images')
-  },
-  filename(req, file, callback) {
-    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
-  },
-})
-
-const upload = multer({ storage: Storage })
-
-app.get('/images', (req, res) => {
-  res.status(200).send('You can post to /api/upload.')
-})
-
-app.post('/images/upload', upload.array('photo', 3), (req, res) => {
-  console.log('file', req.files)
-  console.log('body', req.body)
-  res.status(200).json({
-    message: 'success!',
-  })
-})
-
-
-
-
 
 
 // Middleware
